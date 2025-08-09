a818b6247d2ad69d53de73e267b61fefe09cafa6
apiss2 <apiss2ex@gmail.com>
2025-08-09 19:03:18 +0900

    データセットの作成

---
+++ src/data.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+
+import albumentations as A
+import numpy as np
+import torch
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class SDMDataset(Dataset):
+    def __init__(
+        self,
+        image_dir,
+        label_dir,
+        image_suffix=".jpg",
+        label_suffix=".png",
+        transforms: A.Compose | None = None,
+        num_classes: int = 19,
+        size: int = 256,
+        grayscale: bool = False,
+    ):
+        self.image_dir = Path(image_dir)
+        self.label_dir = Path(label_dir)
+        self.grayscale = grayscale
+        self.num_classes = num_classes
+        assert self.image_dir.exists()
+        assert self.label_dir.exists()
+        self.image_pathes = list()
+        self.label_pathes = list()
+        for image_path in self.image_dir.glob(f"*{image_suffix}"):
+            label_path = self.label_dir.joinpath(image_path.stem + label_suffix)
+            if label_path.exists():
+                self.image_pathes.append(image_path)
+                self.label_pathes.append(label_path)
+        if transforms is None:
+            self.transforms = A.Compose(
+                [
+                    A.Resize(size, size, interpolation=Image.Resampling.BILINEAR),
+                    A.HorizontalFlip(),
+                ]
+            )
+
+    def __len__(self):
+        return len(self.image_pathes)
+
+    def __getitem__(self, i: int):
+        image: Image.Image = Image.open(self.image_pathes[i])
+        if self.grayscale:
+            img = np.array(image.convert("L"))
+        else:
+            img = np.array(image.convert("RGB"))
+        mask = np.array(Image.open(self.label_pathes[i]))  # (H, W) int labels
+
+        # Augmentation
+        aug = self.transforms(image=img, mask=mask)
+        img: np.ndarray = aug["image"]
+        mask: np.ndarray = aug["mask"]
+
+        # normalization
+        img = img.astype(np.float32) / 127.5 - 1.0  # [-1, 1]
+        img = torch.from_numpy(img).permute(2, 0, 1)  # (C, H, W)
+
+        # generate one-hot label: (C,H,W)
+        shape = (self.num_classes, mask.shape[0], mask.shape[1])
+        onehot_label = np.zeros(shape, dtype=np.float32)
+        valid = (mask >= 0) & (mask < self.num_classes)
+        zidx, yidx, xidx = mask[valid], np.where(valid)[0], np.where(valid)[1]
+        onehot_label[zidx, yidx, xidx] = 1.0
+        seg = torch.from_numpy(onehot_label)
+
+        return {"image": img, "semantic_label": seg}
